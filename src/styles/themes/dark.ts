bcfe6ea697908191ffa8fee35f1582e41f05e73e
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-02 17:31:40 -0300

    Subindo projeto

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#DB5A42',
+
+    background: '#222',
+    text: '#fff'
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#DB5A42',
+    secondary: '#A57F60',
+
+    background: '#f5f5f5',
+    text: '#333'
+  },
+};
